0c32be4a435ac80f750c9d6073d4f3b14d404f4d
Gilmhar01~ <carlosg.sanya@gmail.com>
2023-01-04 17:33:30 -0600

    se cambio expresion regular email

--- lib/comm/gen_toast_text_field.dart
+++ lib/comm/gen_toast_text_field.dart
@@ -19,6 +19,6 @@ validateCurp(String curp) {
 }
 
 validateEmail(String email){
-  final emailReg = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+  final emailReg = RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
   return emailReg.hasMatch(email);
 }
